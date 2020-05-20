e1e80d7c8b2b73f2974fcdd001c8128e68be20a2
popn1 <63239598+popn1@users.noreply.github.com>
2020-05-20 23:59:51 +0300

    obshiy

---
+++ индивидуальное задание1.bat
@@ -0,0 +1,26 @@
+
+md popina
+cd popina
+md nastya
+cd nastya
+md nikolaevna
+cd ..
+pause
+echo> 04042000.txt
+cd nastya
+cd nikolaevna
+echo> lol.txt
+cd ..
+cd ..
+cd ..
+pause
+del popina /S/Q/F
+pause
+cd popina
+cd nastya
+rd nikolaevna
+cd ..
+rd nastya
+cd ..
+rd popina
+pause
